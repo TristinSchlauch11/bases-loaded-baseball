b3e4ea1af3d4db77444345c52c18a639b3c06a9c
Tristin Schlauch <nmbr1hockeyfan@gmail.com>
2025-04-01 13:08:10 -0600

    Completed V0.1.0

--- Game.py
+++ Game.py
@@ -1,11 +1,16 @@
 from Player import Player
+from Team import Team
 import Event as e
 
 class Game():
-    def __init__(self):
+    def __init__(self, home, away):
+        self.__teams = self.__away, self.__home = away, home
         self.__outs = 0
         self.__inning = 1
         self.__half = 0
+        self.__batting = self.__away    # attempting new approach
+        self.__pitching = self.__home   # attempting new approach
+        self.__batinds = [0, 0]
         self.__over = False
 
     def play(self):
@@ -13,7 +18,15 @@ class Game():
         Operates the main gameplay loop
         """
         while not self.__over:
-            self.at_bat(b, p)
+            # get batter and pitcher
+            bat = self.__batting.get_batter(self.__batinds[self.__half])
+            pit = self.__pitching.get_pitcher()
+
+            # execute PA
+            self.at_bat(bat, pit)
+            self.__batinds[self.__half] = (self.__batinds[self.__half] + 1) % 9
+
+            # check game status
             self.__over = self.check_game()
             if not self.__over and self.__outs == 3:
                 self.update_inning()
@@ -40,15 +53,12 @@ class Game():
         elif result == 6:
             print("strikeout")
             self.__outs += 1
-            print(f"OUTS: {self.__outs}")
         elif result == 7:
             print("ground out")
             self.__outs += 1
-            print(f"OUTS: {self.__outs}")
         elif result == 8:
             print("air out")
             self.__outs += 1
-            print(f"OUTS: {self.__outs}")
 
     def update_inning(self):
         """
@@ -56,11 +66,13 @@ class Game():
         """
         self.__inning += self.__half
         self.__half = (self.__half + 1) % 2
+        self.__batting = self.__teams[self.__half]
+        self.__pitching = self.__teams[self.__half - 1]
         self.__outs = 0
         if self.__half == 0:
-            print(f"TOP {self.__inning}")
+            print(f"\nTOP {self.__inning}")
         else:
-            print(f"BOT {self.__inning}")
+            print(f"\nBOT {self.__inning}")
 
     def check_game(self):
         """
@@ -71,8 +83,29 @@ class Game():
         return self.__inning >= 9 and self.__half == 1 and self.__outs >= 3
 
 # for testing purposes
-b = Player("Gunnar", "Henderson")
-p = Player("Paul", "Skenes")
+bluejays = Team("Blue Jays")
+bluejays.add_batter(Player("Alejandro", "Kirk"))
+bluejays.add_batter(Player("Vladamir", "Guerrero Jr."))
+bluejays.add_batter(Player("Andres", "Gimenez"))
+bluejays.add_batter(Player("Bo", "Bichette"))
+bluejays.add_batter(Player("Ernie", "Clement"))
+bluejays.add_batter(Player("Davis", "Schneider"))
+bluejays.add_batter(Player("Daulton", "Varsho"))
+bluejays.add_batter(Player("George", "Springer"))
+bluejays.add_batter(Player("Anthony", "Santander"))
+bluejays.add_pitcher(Player("Jose", "Berrios"))
 
-g = Game()
+swansons = Team("Swansons")
+swansons.add_batter(Player("Sherry", "Lee"))
+swansons.add_batter(Player("Tanner", "Mergle"))
+swansons.add_batter(Player("Teresa", "Three"))
+swansons.add_batter(Player("Tristin", "Schlauch"))
+swansons.add_batter(Player("Sungwoo", "Byun"))
+swansons.add_batter(Player("William", "Blimke"))
+swansons.add_batter(Player("Ryan", "Chan"))
+swansons.add_batter(Player("Reed", "Drinkle"))
+swansons.add_batter(Player("Jason", "Nine"))
+swansons.add_pitcher(Player("Kris", "Clements"))
+
+g = Game(bluejays, swansons)
 g.play()
