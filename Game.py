379f2237d3df3d669d42f89ee6f5dd8f51c2fb36
Tristin Schlauch <nmbr1hockeyfan@gmail.com>
2025-03-31 18:06:49 -0600

    Created Team.py, updated Game.py for framework of 9-inning, 3-outs game

--- Game.py
+++ Game.py
@@ -3,17 +3,29 @@ import Event as e
 
 class Game():
     def __init__(self):
-        self.results = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0}     # TESTING
+        self.__outs = 0
+        self.__inning = 1
+        self.__half = 0
+        self.__over = False
+
+    def play(self):
+        """
+        Operates the main gameplay loop
+        """
+        while not self.__over:
+            self.at_bat(b, p)
+            self.__over = self.check_game()
+            if not self.__over and self.__outs == 3:
+                self.update_inning()
 
     def at_bat(self, bat, pit):
         """
         Executes an at-bat between Batter bat and Pitcher pit
         """
-        print("Batter: " + bat.get_last())
-        print("Pitcher: " + pit.get_last())
+        print(f"Batter: {bat.get_last()}")
+        print(f"Pitcher: {pit.get_last()}")
         
         result = e.ab(bat, pit)
-        self.results[result] += 1       # TESTING
 
         if result == 1:
             print("single")
@@ -27,17 +39,40 @@ class Game():
             print("walk")
         elif result == 6:
             print("strikeout")
+            self.__outs += 1
+            print(f"OUTS: {self.__outs}")
         elif result == 7:
             print("ground out")
+            self.__outs += 1
+            print(f"OUTS: {self.__outs}")
         elif result == 8:
             print("air out")
+            self.__outs += 1
+            print(f"OUTS: {self.__outs}")
+
+    def update_inning(self):
+        """
+        Updates the game after an inning has ended
+        """
+        self.__inning += self.__half
+        self.__half = (self.__half + 1) % 2
+        self.__outs = 0
+        if self.__half == 0:
+            print(f"TOP {self.__inning}")
+        else:
+            print(f"BOT {self.__inning}")
+
+    def check_game(self):
+        """
+        Checks if the game is over
+
+        FOR NOW, this is a dummy method that needs to be updated later
+        """
+        return self.__inning >= 9 and self.__half == 1 and self.__outs >= 3
 
 # for testing purposes
 b = Player("Gunnar", "Henderson")
 p = Player("Paul", "Skenes")
 
 g = Game()
-for i in range(100000):
-    g.at_bat(b, p)
-
-print(g.results)
+g.play()
---
+++ Team.py
@@ -0,0 +1,5 @@
+from Player import Player
+
+class Team():
+    def __init__(self):
+        pass
