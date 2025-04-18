29758bda765c96a571c5245e70cb7fd034d527e1
Tristin Schlauch <nmbr1hockeyfan@gmail.com>
2025-04-18 16:33:19 -0600

    Added a bullpen and print_bullpen

--- Notes.md
+++ Notes.md
@@ -6,3 +6,4 @@ This file will be used to track notes, things to do, and things to fix in the pr
     - These numbers are adjusted for AVG, not H% (i.e. H/PA)
 
 V0.1.0 Notes: Can I store game-time team information (like score, bat_ind, etc.) into a dictionary? Could help ease information retrieval. Think about implementing this in a future version.
+V0.4.0 Notes: I am implementing this dictionary now. It will be put into the Game class and will prevent me from needing to call get_pitcher every at-bat (or even every half-inning)
--- Team.py
+++ Team.py
@@ -5,6 +5,7 @@ class Team():
         self.__name = name
         self.__lineup = []
         self.__rotation = []
+        self.__bullpen = []
 
     def get_name(self):
         """
@@ -12,6 +13,13 @@ class Team():
         """
         return self.__name
     
+    def print_bullpen(self):
+        """
+        Prints the pitchers in the bullpen to the screen
+        """
+        for i in range(len(self.__bullpen)):
+            print(f"{i+1}. {self.__bullpen[i].get_last()}")
+
     def get_batter(self, index):
         """
         Returns the Batter from the Team's lineup at the specified index
@@ -32,8 +40,13 @@ class Team():
         """
         self.__lineup.append(bat)
     
-    def add_pitcher(self, pit):
+    def add_pitcher(self, pit, is_sp):
         """
-        Adds Pitcher pit to the Team
+        Adds Pitcher pit to the Team. Will add the pitcher to the rotation if a starter,
+        bullpen if they are a reliever
         """
-        self.__rotation.append(pit)
+        # Will likely change the way that the players get sorted into the rotation/bullpen
+        if is_sp:
+            self.__rotation.append(pit)
+        else:
+            self.__bullpen.append(pit)
